b9faa4ad13783928a67f3498e95c6670aa218cb9
Ngô Minh Trung <trung.nm226004@sis.hust.edu.vn>
2026-01-16 11:06:39 +0700

    update_judgement_missing

--- src/judgement.py
+++ src/judgement.py
@@ -3,7 +3,7 @@ from .utils import generate_response
 
 def generate_judgement(ctrait, text, explanation1, explanation2, model_name, max_new_tokens: int = None, pipeline = None):
     sys_p, usr_p = judgement_prompt_build(ctrait, text, explanation1, explanation2)
-    return generate_response(sys_p, usr_p, model=model_name, max_new_tokens=max_new_tokens)
+    return generate_response(sys_p, usr_p, model=model_name, max_new_tokens=max_new_tokens, pipeline=pipeline)
 
 def extract_judgement_prediction(judgement_response: str) -> str:
     for line in judgement_response.splitlines():
